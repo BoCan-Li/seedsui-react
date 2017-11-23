a13385f413ea6c8f10842b828a7ff485337fc912
wangmingzhu <wangmingzhu@waiqin365.com>
2017-11-23 15:44:55 +0800

    'fastclick'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Router from '@/router/router.js'
+import FastClick from 'fastclick';
+FastClick.attach(document.body);
+
 ReactDOM.render(
   <Router />,
   document.getElementById('root')
