33275f6139866fc3c7b6c92c165f47a132f002e9
wangmingzhu <wangmingzhu@waiqin365.com>
2017-12-17 22:31:33 +0800

    'aa'

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import {withRouter} from 'react-router'
 import logo from './logo.svg';
 import './About.css';
-
+@withRouter
 class About extends Component {
+  onClickBack = () => {
+    // const { match, location, history } = this.props
+    const {history} = this.props;
+    history.goBack();
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +17,7 @@ class About extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={this.onClickBack}>返回</button>
         </p>
       </div>
     );
