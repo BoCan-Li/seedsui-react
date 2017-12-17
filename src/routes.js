05bdf44b1b6decf867f782fe14513a4d75d697d1
wangmingzhu <wangmingzhu@waiqin365.com>
2017-12-17 21:05:42 +0800

    'aa'

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-import Home from 'containers/Home'; // 登录后默认首页
-import About from 'containers/About'; // 登录后默认首页
-
-// redux
+// Redux
 import { Provider } from 'react-redux'
 import store from 'store/index.js'
+
+// Containers
+import Home from 'containers/Home'; // 登录后默认首页
+import About from 'containers/About'; // 登录后默认首页
 
 const Routes = () => (
   <Provider store={store}>
