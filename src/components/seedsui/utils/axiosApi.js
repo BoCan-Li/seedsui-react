8bf7d28ee8a047f2c88c512193cd27352518dcba
王明柱 <wangmingzhu@wangmingzhudeMacBook-Pro.local>
2018-04-21 15:39:43 +0800

    less注释简化

--- src/components/seedsui/utils/axiosApi.js
+++ src/components/seedsui/utils/axiosApi.js
@@ -8,7 +8,8 @@ axios.defaults.headers.post['Content-Type'] = 'application/jsoncharset=UTF-8'
 const env = process.env.NODE_ENV
 
 if (env === 'development') {
-  axios.defaults.baseURL = `http://localhost:4040/api` // 加上/api,为了匹配代理过滤器,使用代理做跨域
+  // axios.defaults.baseURL = `http://localhost:4040/api` // 加上/api,为了匹配代理过滤器,使用代理做跨域
+  axios.defaults.baseURL = `http://localhost:4040`
   axios.defaults.withCredentials = true
 }
 // 构建get请求参数
