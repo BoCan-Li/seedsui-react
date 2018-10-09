f85fd38a850bb4463dbc994d40452f589c9ed966
wangmingzhu <wangmingzhu@waiqin365.com>
2018-10-09 16:31:34 +0800

    onoff

--- src/demo/App.js
+++ src/demo/App.js
@@ -3,20 +3,13 @@ import Page from '../lib/Page';
 import Header from '../lib/Header';
 import Container from '../lib/Container';
 import Titlebar from '../lib/Titlebar';
-import Dropdown from '../lib/Dropdown';
-import MenuTree from '../lib/MenuTree';
+import OnOff from '../lib/OnOff';
 
-const MenuTreeStyle = {
-  position: 'absolute',
-  top: '300px',
-  left: '0',
-  bottom: '0'
-}
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: "1234"
+      value: true
     }
   }
   componentDidMount() {
@@ -31,9 +24,9 @@ export default class App extends Component {
     var scrollTop = e.currentTarget.scrollTop;
     console.log(scrollTop)
   }
-  onChange = (value) => {
+  onChange = () => {
     this.setState({
-      value
+      value: !this.state.value
     })
   }
   changeDate = () => {
@@ -47,15 +40,12 @@ export default class App extends Component {
     })
   }
   render() {
-    const list = [{"id":"-1","show_order":-1,"caption":"全部","children":[],"parent_id":"-2"},{"id":"5884112927677396749","show_order":0,"caption":"百饮","children":[],"parent_id":"-1"},{"id":"5170817935962998220","show_order":0,"caption":"业务","children":[],"parent_id":"-1"},{"id":"5560958318574727662","show_order":1,"caption":"PET中包装","children":[],"parent_id":"-1"},{"id":"9199989753697871223","show_order":2,"caption":"乘用车","children":[{"id":"8182192418836738254","show_order":0,"caption":"家轿","parent_id":"9199989753697871223","children":[{"id":"1234","caption":"家轿","parent_id":"8182192418836738254"}]},{"id":"4719934568248841585","show_order":0,"caption":"SUV","parent_id":"9199989753697871223"}],"parent_id":"-1"},{"id":"6381663369076352142","show_order":3,"caption":"康师傅方便面","children":[{"id":"6162698864252245743","show_order":0,"caption":"经典袋面单包","parent_id":"6381663369076352142"},{"id":"4878138732125492161","show_order":0,"caption":"杯面","parent_id":"6381663369076352142"},{"id":"9006409737235548571","show_order":0,"caption":"开心桶","parent_id":"6381663369076352142"},{"id":"4753429730191617176","show_order":0,"caption":"高汤","parent_id":"6381663369076352142"},{"id":"8025475447269485158","show_order":0,"caption":"大食袋","parent_id":"6381663369076352142"},{"id":"6848161054921512588","show_order":0,"caption":"经典五连","parent_id":"6381663369076352142"}],"parent_id":"-1"},{"id":"1","show_order":4,"caption":"香烟","children":[],"parent_id":"-1"},{"id":"6321208477511373988","show_order":5,"caption":"CAN拉罐","children":[],"parent_id":"-1"}];
-    const dropdownList = [{caption: 'haha', data: list}]
     return <Page style={{ backgroundColor: 'white' }}>
       <Header>
         <Titlebar caption="SeedsUI" rButtons={[{ caption: 'haha' , onClick: this.showDialog}]} />
-        <Dropdown list={dropdownList}/>
       </Header>
       <Container>
-        <MenuTree list={list} style={MenuTreeStyle} onClick={this.onClick} onScroll={this.onScrollMenu} activeId={this.state.value}/>
+        <OnOff onClick={this.onChange} checked={this.state.value}/>
       </Container>
     </Page>
   }
