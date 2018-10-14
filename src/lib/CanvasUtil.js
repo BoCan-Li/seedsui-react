0fc11beb170e908f0b134e2466113d4448af6f8f
wangmingzhu <wangmingzhu@waiqin365.com>
2018-10-14 17:30:31 +0800

    Jcrop

---
+++ src/lib/CanvasUtil.js
@@ -0,0 +1,52 @@
+var CanvasUtil = {
+  cropImg: function (opts = {}) {
+    // 参数校验
+    var errMsg = ''
+    if (isNaN(opts.x)) {
+      errMsg = 'SeedsUI Error:CanvasUtil类cropImg裁切方法x参数不正确'
+      console.warn(errMsg)
+      return errMsg
+    }
+    if (isNaN(opts.y)) {
+      errMsg = 'SeedsUI Error:CanvasUtil类cropImg裁切方法y参数不正确'
+      console.warn(errMsg)
+      return errMsg
+    }
+    if (isNaN(opts.width)) {
+      errMsg = 'SeedsUI Error:CanvasUtil类cropImg裁切方法width参数不正确'
+      console.warn(errMsg)
+      return errMsg
+    }
+    if (isNaN(opts.height)) {
+      errMsg = 'SeedsUI Error:CanvasUtil类cropImg裁切方法width参数不正确'
+      console.warn(errMsg)
+      return errMsg
+    }
+    var suffix = opts.suffix || 'image/png'
+    var quality = opts.quality || 0.92
+    // 开始裁切
+    var canvas = document.getElementById('imgcrop_canvas')
+    if (!canvas) {
+      canvas = document.createElement('canvas')
+      canvas.setAttribute('id', 'imgcrop_canvas')
+      document.body.appendChild(canvas)
+    }
+    var ctx = canvas.getContext('2d')
+    canvas.width = opts.width;
+    canvas.height = opts.height;
+    var img = new Image()
+    img.src = opts.src
+    img.crossOrigin = 'Anonymous'
+    img.addEventListener('load', function () {
+      ctx.drawImage(img, opts.x, opts.y, opts.width, opts.height, 0, 0, opts.width, opts.height)
+      // 成功回调
+      try {
+        if (opts.onSuccess) opts.onSuccess(canvas.toDataURL(suffix, quality))
+      } catch (error) {
+        if (opts.onError) opts.onError(error)
+      }
+    }, false)
+  }
+}
+
+export default CanvasUtil
---
+++ src/lib/Jcrop/Jcrop.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Instance from 'jcrop';
+import 'jcrop/dist/jcrop.css'; // Jcrop
+
+export default class Jcrop extends Component {
+  static propTypes = {
+    src: PropTypes.string,
+    rect: PropTypes.array, // [10,10,100,100]
+    scale: PropTypes.array, // [.7,.5]
+    options: PropTypes.object,
+    onChange: PropTypes.func
+  }
+  static defaultProps = {
+    scale: [.7,.5],
+    options: {
+      multi: false
+    }
+  }
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount () {
+    this.update();
+  }
+  formatPos = (argPos) => {
+    var pos = argPos;
+    for (let n in argPos) {
+      if (n === 'h') pos['height'] = argPos[n];
+      else if (n === 'w') pos['width'] = argPos[n];
+    }
+    return pos;
+  }
+  update = () => {
+    if (!this.props.src) return;
+    const {rect, scale} = this.props;
+    Instance.load(this.$el).then(img => {
+      var jcp = Instance.attach(img, this.props.options);
+      var frame = null;
+      let pos = {};
+      // 按尺寸创建
+      if (rect) {
+        pos = {
+          x: rect[0],
+          y: rect[1],
+          w: rect[2],
+          h: rect[3]
+        }
+        frame = Instance.Rect.create(pos.x, pos.y, pos.w, pos.h);
+        jcp.newWidget(frame);
+      // 按比例创建
+      } else {
+        frame = Instance.Rect.sizeOf(jcp.el);
+        pos = frame.scale(...scale).center(frame.w,frame.h);
+        jcp.newWidget(pos);
+      }
+      if (this.props.onChange) this.props.onChange({pos: this.formatPos(pos), src: this.props.src});
+      jcp.listen('crop.change', (widget, e) => {
+        pos = this.formatPos(widget.pos);
+        if (this.props.onChange) this.props.onChange({pos, src: this.props.src});
+      });
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+  render() {
+    const {src, style, className} = this.props;
+    return (
+      <img alt="" ref={(el) => {this.$el = el;}} className={className} style={Object.assign({maxWidth: '100%'}, style)} src={src}/>
+    );
+  }
+}
