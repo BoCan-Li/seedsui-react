ae0630c343b74a31fbc5de6d5a9dc032bbe48f2d
wangmingzhu <wangmingzhu@waiqin365.com>
2018-12-04 20:31:40 +0800

    apiaxios

--- src/lib/ApiAxios.js
+++ src/lib/ApiAxios.js
@@ -2,10 +2,6 @@
 import axios from 'axios'
 
 // 封装成Api类
-function buildParams (params) {
-  if (params && !params.data) return {data: params}
-  return params
-}
 const Api = {
   onError: function (error) {
     console.warn(error)
@@ -20,18 +16,25 @@ const Api = {
     axios.defaults.baseURL = baseURL
   },
   post: function (url, params) {
-    return axios.post(url, params)
+    return axios({
+      url: url,
+      method: 'post',
+      data: params
+    })
   },
   get: function (url, params) {
-    const newParams = buildParams(params)
-    return axios.get(url, newParams)
+    return axios({
+      url: url,
+      method: 'get',
+      data: params
+    })
   },
-  all: function (requests) {
+  all: function (requests) { // [{url: '', method: 'post|get', params: {}}]
     const methods = requests.map((request) => {
       if (request.method === 'post') {
-        return this.post(request.url, request.data || null)
+        return this.post(request.url, request.params || null)
       } else {
-        return this.get(request.url, request.data || null)
+        return this.get(request.url, request.params || null)
       }
     })
     return axios.all(methods)
@@ -40,7 +43,6 @@ const Api = {
 
 // axios 默认配置
 // axios.defaults.timeout = 5000
-// 设置头
 axios.defaults.headers.post['Content-Type'] = 'application/jsoncharset=UTF-8'
 
 // 构建get请求参数
@@ -49,28 +51,39 @@ function buildGetUrl (url, params) {
   if (result) return url + '?' + result
   return url
 }
+
 // 请求拦截器
 axios.interceptors.request.use(config => {
   var conf = Object.clone(config)
   // 设置请求数据
-  var data = ''
+  var req = {}
   try {
-    data = JSON.parse(config.data)
+    req = JSON.parse(config.data)
   } catch (e) {
-    data = config.data
+    req = config.data
   }
-  if (data && data.data) {
-    conf.data = data.data
+  if (req && req.data) {
+    conf.data = req.data
   }
   // 设置头
-  var head = ''
-  if (data && data.head) {
-    head = config.data.head
+  var head = {}
+  if (req && req.head) {
+    head = req.head
     Object.keys(head).forEach(key => {
       var value = head[key]
       conf.headers[key] = value
     })
   }
+  // 设置options
+  var options = {}
+  if (req && req.options) {
+    options = req.options
+    Object.keys(options).forEach(key => {
+      var value = options[key]
+      conf[key] = value
+    })
+  }
+  // 设置get请求
   if (config.method === 'get') {
     conf.url = buildGetUrl(conf.url, conf.data)
   }
@@ -78,6 +91,7 @@ axios.interceptors.request.use(config => {
 }, error => {
   return Promise.reject(error)
 })
+
 // 响应拦截器
 axios.interceptors.response.use(response => {
   let result = response.data || response
