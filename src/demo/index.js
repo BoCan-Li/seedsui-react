b0cc26c9376c30da71b83b77ddffd00ab91fc977
wangmingzhu <wangmingzhu@waiqin365.com>
2018-12-21 15:48:56 +0800

    a

--- src/demo/index.js
+++ src/demo/index.js
@@ -17,8 +17,15 @@ import FastClick from '../lib/FastClick';
 if (Device.platform === 'dinghuo' || Device.platform === 'waiqin') {
   FastClick.attach(document.getElementById('root'));
 }
+
 // 适配iPhoneX和andriod5.0以下的手机
 Device.adapterMobile();
+
+// 处理客户端中安卓5.0以下手机输入法上弹隐藏后,界面显示错位的问题
+if ((Device.platform === 'dinghuo' || Device.platform === 'waiqin') && Device.os === 'andriod' && Device.osVersion < '5.0') {
+  document.getElementById('root').style.position = 'fixed';
+}
+
 // 动态加载桥接库
 Device.dynamicLoadBridge(() => {
   ReactDOM.render(<App />, document.getElementById('root'));
