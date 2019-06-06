3523f5039aee4ea8ae9dfb21bf0f3f968ec1ebfa
wangmingzhu <wangmingzhu@waiqin365.com>
2019-06-06 19:27:38 +0800

    [2.1.7]

--- src/lib/bridge/index.js
+++ src/lib/bridge/index.js
@@ -2,17 +2,19 @@ import Device from './../Device'
 
 var Bridge = {}
 if (Device.platform === 'weixin') {
-  Bridge = require('./bridgeWeixin').default
+  Bridge = require('./bridgeWeixin')
 } else if (Device.platform === 'weixinwork') {
-  Bridge = require('./bridgeWeixin').default
+  Bridge = require('./bridgeWeixin')
 } else if (Device.platform === 'dinghuo') {
-  Bridge = require('./bridgeDinghuo').default
+  Bridge = require('./bridgeDinghuo')
   Bridge.config()
 } else if (Device.platform === 'waiqin') {
-  Bridge = require('./bridgeWaiqin').default
+  Bridge = require('./bridgeWaiqin')
   Bridge.config()
 } else {
-  Bridge = require('./bridgeBrowser').default
+  Bridge = require('./bridgeBrowser')
 }
+if (Bridge.default) Bridge = Bridge.default
+if (Bridge.config) Bridge.config()
 
 export default Bridge
