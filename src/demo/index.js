78fe61f77c94f82342ef1592d970c8e77e7017a1
wangmingzhu <wangmingzhu@waiqin365.com>
2019-09-12 14:18:02 +0800

    src/index

--- src/demo/index.js
+++ src/demo/index.js
@@ -13,13 +13,12 @@ import '../lib/PrototypeDate.js';
 import Device from '../lib/Device';
 import FastClick from '../lib/FastClick';
 
+var root = document.getElementById('root');
 // ios外勤客户端UIWebview点击慢的问题
-if (Device.platform === 'waiqin') {
-  var root = document.getElementById('root');
-  if (root && Device.os === 'ios') {
-    FastClick.attach(root);
-  }
+if (root && Device.platform === 'waiqin' && Device.os === 'ios') {
+  FastClick.attach(root);
 }
+
 // 动态加载桥接库
 Device.dynamicLoadBridge(() => {
   ReactDOM.render(<App />, document.getElementById('root'));
