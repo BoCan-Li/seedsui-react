068fd632e9068665e34e6b7d896330505f23709c
wangmingzhu <wangmingzhu@waiqin365.com>
2019-10-14 20:42:27 +0800

    标注系统

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,38 +4,6 @@ import {render} from 'react-dom'
 import {Page, Header, Titlebar, Container, Vott} from '../../src'
 
 class Demo extends Component {
-  // 创建矩形
-  createRect = (attr) => {
-    var svg = this.createSvg('svg', {
-      'style': 'width:100%;height:100%;',
-      'viewBox': '0 0 100 100'
-    });
-    
-    var circle = this.createSvg('rect', {  
-      'x': '1',  
-      'y': '1',  
-      'width': '20',
-      'height': '20', 
-      'style': 'fill:#ff6666;stroke-width:1;stroke:rgb(255,0,0)'
-    });
-    svg.appendChild(circle);
-    return svg;
-  }
-  // 创建svg相关元素  
-  createSvg = (tag, attr) => {
-    if (!document.createElementNS) return // 防止IE8报错  
-    var svg = document.createElementNS('http://www.w3.org/2000/svg', tag)
-    for (var key in attr) {
-      switch (key) {
-        case 'xlink:href': // 文本路径添加属性特有
-          svg.setAttributeNS('http://www.w3.org/1999/xlink', key, attr[key])
-          break
-        default:
-          svg.setAttribute(key, attr[key])
-      }
-    }
-    return svg;
-  }
   componentDidMount () {
   }
   render() {
--- src/Vott/instance.js
+++ src/Vott/instance.js
@@ -6,6 +6,9 @@ var Vott = function (container, params) {
   -------------------- */
   var defaults = {
     containerClass: 'vott-container',
+    svgClass: 'vott-svg',
+    shapeClass: 'vott-shape',
+    shapeActiveClass: 'active',
     /*
     callbacks
     onInit:function(s)
@@ -31,6 +34,9 @@ var Vott = function (container, params) {
     return
   }
 
+  // Svg
+  s.svg = null
+
   // 更新DOM
   s.update = function () {
     if (!s.container) return
@@ -39,6 +45,80 @@ var Vott = function (container, params) {
   /* --------------------
   Methods
   -------------------- */
+  // 更新svg忏悔
+  s.updateSvg =  function (svg, attr) {
+    if (!attr) return
+    for (var key in attr) {
+      switch (key) {
+        case 'xlink:href': // 文本路径添加属性特有
+          svg.setAttributeNS('http://www.w3.org/1999/xlink', key, attr[key])
+          break
+        default:
+          svg.setAttribute(key, attr[key])
+      }
+    }
+  }
+  // 创建svg相关元素  
+  s.createSvg = function (tag, attr) {
+    if (!document.createElementNS) return // 防止IE8报错  
+    var svg = document.createElementNS('http://www.w3.org/2000/svg', tag)
+    s.updateSvg(svg, attr)
+    return svg
+  }
+  
+  // 创建矩形
+  s.createShape = function (svg, shapeName, attr) {
+    // 创建svg容器
+    if (!svg) {
+      svg = this.createSvg('svg', {
+        'class': s.params.svgClass,
+        'viewBox': '0,0,' + s.container.clientWidth + ',' + s.container.clientHeight // 视窗大小决定里层的像素, 类似rem
+      })
+    }
+    // 创建形状
+    var shape = this.createSvg(shapeName || 'rect', attr || {  
+      'x': 0,
+      'y': 0,
+      'width': 20,
+      'height': 20, 
+      'class': s.params.shapeClass + ' ' + s.params.shapeActiveClass
+    })
+    svg.shape = shape
+    svg.appendChild(shape)
+
+    return svg
+  }
+  // 拖动形状大小
+  s.scaleShape = function (target, x, y, w, h) {
+    var startX = x
+    var startY = y
+    var width = w
+    var height = h
+    // 如果宽度为正数, 用户逆向缩放, 则修改x坐标与宽度两项
+    if (w > 0) {
+      startX = x - w
+    }
+    // 如果高度为正数, 用户逆向缩放, 则修改y坐标与高度两项
+    if (h > 0) {
+      startY = y - h
+    }
+    s.updateSvg(target, {
+      'x': startX,
+      'y': startY,
+      'width': Math.abs(width),
+      'height': Math.abs(height),
+      'class': s.params.shapeClass + ' ' + s.params.shapeActiveClass
+    })
+  }
+  // 移动形状
+  s.moveShape = function (target, x, y, w, h) {
+    var startX = x
+    var startY = y
+    s.updateSvg(target, {
+      'x': startX - w,
+      'y': startY - h
+    })
+  }
   /* --------------------
   Touch Events
   -------------------- */
@@ -74,9 +154,8 @@ var Vott = function (container, params) {
   }
   // Touch信息
   s.touches = {
-    direction: 0,
-    vertical: 0,
-    horizontal: 0,
+    target: null, // 操作的对象
+    type: 'new', // 类型: 创建 'new', 移动 'move', 缩放 'size'
     startX: 0,
     startY: 0,
     currentX: 0,
@@ -84,27 +163,75 @@ var Vott = function (container, params) {
     endX: 0,
     endY: 0,
     diffX: 0,
-    diffY: 0,
-    posX: 0
+    diffY: 0
   }
   s.onTouchStart = function (e) {
-    console.log(e)
     s.container.addEventListener('touchmove', preventDefault, false)
-    s.touches.startX = e.clientX || e.touches[0].clientX
-    s.touches.startY = e.clientY || e.touches[0].clientY
+    s.touches.startX = e.offsetX || e.touches[0].clientX
+    s.touches.startY = e.offsetY || e.touches[0].clientY
+
+    // 允许拖动
+    s.moveEnd = false
+    // 完成后, 将选中状态置为普通状态
+    if (s.touches.target) {
+      s.touches.target.classList.remove(s.params.shapeActiveClass)
+    }
+
+    if (e.target.classList.contains(s.params.shapeClass)) { // 点击形状
+      e.target.classList.add(s.params.shapeActiveClass)
+      s.touches.target = e.target
+      s.svg = e.target.parentNode
+
+      s.touches.type = 'move'
+    } else { // 点击svg或者空白
+      if (e.target.classList.contains(s.params.svgClass)) s.svg = e.target
+      s.svg = s.createShape(s.svg, 'rect', {  
+        'x': s.touches.startX,  
+        'y': s.touches.startY,  
+        'width': '1',
+        'height': '1', 
+        'class': s.params.shapeClass + ' ' + s.params.shapeActiveClass
+      })
+      s.touches.target = s.svg.shape
+      s.container.appendChild(s.svg)
+
+      s.touches.type = 'new'
+    }
   }
   s.onTouchMove = function (e) {
-    console.log(e)
-    s.touches.currentX = e.clientX || e.touches[0].clientX
-    s.touches.currentY = e.clientY || e.touches[0].clientY
+    if (s.moveEnd) return
+    if (!s.touches.target) return
+    s.touches.currentX = e.offsetX || e.touches[0].clientX
+    s.touches.currentY = e.offsetY || e.touches[0].clientY
     s.touches.diffX = s.touches.startX - s.touches.currentX
     s.touches.diffY = s.touches.startY - s.touches.currentY
+    
+    // 缩放尺寸
+    if (s.touches.type === 'new') {
+      s.scaleShape(s.touches.target, s.touches.startX, s.touches.startY, s.touches.diffX, s.touches.diffY)
+    } else if (s.touches.type === 'move') {
+      // 记录开始移动位置
+      if (!s.touches.moveStartX) s.touches.moveStartX = s.touches.target.getAttribute('x')
+      if (!s.touches.moveStartY) s.touches.moveStartY = s.touches.target.getAttribute('y')
+      s.moveShape(s.touches.target, s.touches.moveStartX, s.touches.moveStartY, s.touches.diffX, s.touches.diffY)
+    }
   }
   s.onTouchEnd = function (e) {
-    console.log(e)
     if (s.params.stopPropagation) e.stopPropagation() // 此属性与FastClick冲突
-    s.touches.endX = e.clientX || e.changedTouches[0].clientX
-    s.touches.endY = e.clientY || e.changedTouches[0].clientY
+    s.touches.endX = e.offsetX || e.changedTouches[0].clientX
+    s.touches.endY = e.offsetY || e.changedTouches[0].clientY
+
+    // 清空记录的移动位置
+    if (s.touches.moveStartX) s.touches.moveStartX = null
+    if (s.touches.moveStartY) s.touches.moveStartY = null
+
+    // 单击事件
+    if (Math.abs(s.touches.startX - s.touches.endX) < 6 && Math.abs(s.touches.startY - s.touches.endY) < 6) {
+      s.touches.target.parentNode.removeChild(s.touches.target)
+      s.touches.target = null
+    }
+
+    s.moveEnd = true
   }
   /* --------------------
   Init
