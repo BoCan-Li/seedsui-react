ae087328e3e602b5e017288c8d54e1d454f82c14
wangmingzhu <wangmingzhu@waiqin365.com>
2020-04-07 20:34:15 +0800

    修复文件上传的问题

--- src/ApiAxios/ApiAxios.js
+++ src/ApiAxios/ApiAxios.js
@@ -5,6 +5,20 @@ import axios from 'axios'
 // axios.defaults.timeout = 5000
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8'
 
+// response中提取result
+function getResult (response) {
+  let result = response.data || response
+  if (typeof result === 'string') {
+    try {
+      return JSON.parse(result)
+    } catch (error) {
+      console.log('result转换JSON失败' + error)
+      return result
+    }
+  }
+  return result
+}
+
 // 构建get请求参数, get请求需要把url和data拼接起来
 function buildGetUrl (url, params) {
   if (!params || Object.isEmptyObject(params)) {
@@ -36,73 +50,29 @@ function jsonp (url) {
 }
 
 // 表单上传
-function formUpload (url, originFile) {
+function formUpload (url, options) {
   return new Promise((resolve) => {
-    const frameId = '_seedsui_iframe_id_';
-    if (document.getElementById(frameId)) {
-      resolve({
-        code: '0',
-        message: '任务进行中, 请按顺序上传'
-      })
-      return
-    }
-    if (!originFile) { // file文件框
+    if (!options.file) { // file文件框
+      console.warn('没有找到options.file, 无法上传')
       resolve({
         code: '0',
         message: '没有找到options.file, 无法上传'
       })
       return
     }
-    // file
-    var file = originFile.cloneNode(true)
-    // iframe
-    var iframe = document.createElement('iframe')
-    iframe.setAttribute('id', frameId)
-    iframe.setAttribute('name', frameId)
-    document.body.appendChild(iframe)
-    // form
-    var form = document.createElement('form')
-    form.setAttribute('action', axios.defaults.baseURL + url)
-    form.setAttribute('method', 'POST')
-    form.enctype = 'multipart/form-data'
-    form.setAttribute('target', frameId)
-    form.appendChild(file)
-    document.body.appendChild(form)
-    form.submit()
-    document.getElementById(frameId).addEventListener('load', () => {
-      // 构建数据
-      var response = {}
-      var io = document.getElementById(frameId)
-      try {
-        if (io.contentWindow) {
-          response.responseText = io.contentWindow.document.body ? io.contentWindow.document.body.innerHTML : null
-          response.responseXML = io.contentWindow.document.XMLDocument ? io.contentWindow.document.XMLDocument : io.contentWindow.document
-        } else {
-          if (io.contentDocument) {
-            response.responseText = io.contentDocument.document.body ? io.contentDocument.document.body.innerHTML : null
-            response.responseXML = io.contentDocument.document.XMLDocument ? io.contentDocument.document.XMLDocument : io.contentDocument.document
-          }
-        }
-        // 返回数据转成json
-        try {
-          resolve(JSON.parse(response.responseText))
-        } catch (error) {
-          console.log('result转换JSON失败' + error)
-          resolve({
-            code: '0',
-            message: '返回数据格式异常, 请稍后再试'
-          })
-        }
-      } catch (err) {
-        console.log(err)
-        resolve({
-          code: '0',
-          message: '上传异常, 请稍后再试'
-        })
-      }
-      document.body.removeChild(form)
-      document.body.removeChild(iframe)
-    }, false)
+    // 上传数据
+    let formData = new FormData()
+    formData.append('file', options.file.files[0])
+    // 发送请求
+    const instance = axios.create({
+      withCredentials: true
+    })
+    instance.post(url, formData).then((response) => {
+      resolve(getResult(response))
+    }).catch((error) => {
+      if (Api.fail) Api.fail(error)
+      return Promise.reject(error)
+    })
   })
 }
 
@@ -118,31 +88,13 @@ const Api = {
     let {method, head, data, ...options} = params
     // 设置method
     if (method !== 'get' && method !== 'post' && method !== 'jsonp' && method !== 'upload' && method !== 'form-upload') method = 'get'
-    // 如果是表单文件上传
-    if (method === 'form-upload') {
-      return formUpload(url, options.file)
-    }
     // 如果是jsonp, 则其它参数都没有意义了
     if (method === 'jsonp') {
       return jsonp(url)
     }
     // 如果是上传文件
     if (method === 'upload') {
-      if (!options.file) { // file文件框
-        console.warn('没有找到options.file, 无法上传')
-        return new Promise((resolve) => {
-          resolve({
-            code: '0',
-            message: '没有找到options.file, 无法上传'
-          })
-        })
-      }
-      head = {'Content-Type': 'multipart/form-data'}
-      method = 'POST'
-      // 上传数据
-      let formData = new FormData()
-      formData.append('file', options.file.files[0])
-      data = formData
+      return formUpload(buildGetUrl(url, data), options)
     }
     return axios({
       url: method === 'get' ? buildGetUrl(url, data) : url,
@@ -173,16 +125,7 @@ const Api = {
 
 // 响应拦截器
 axios.interceptors.response.use(response => {
-  let result = response.data || response
-  if (typeof result === 'string') {
-    try {
-      return JSON.parse(result)
-    } catch (error) {
-      console.log('result转换JSON失败' + error)
-      return result
-    }
-  }
-  return result
+  return getResult(response)
 }, error => {
   if (Api.fail) Api.fail(error)
   return Promise.reject(error)
