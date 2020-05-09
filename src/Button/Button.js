15f7e17bdabd240930ac6dd4c77defd275a687cf
wangmingzhu <wangmingzhu@waiqin365.com>
2020-05-09 17:36:41 +0800

    Button和Picker

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,24 +1,10 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+const Button = forwardRef(({
+  children,
+  ...others
+}, ref) =>  {
+  return <a ref={ref} {...others} className={'button' + (others.className ? ' ' + others.className : '')}>
+    {children}
+  </a>
+})
 
-export default class Button extends Component {
-  static propTypes = {
-    children: PropTypes.node,
-  }
-  static defaultProps = {
-  }
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const {
-      children,
-      ...others
-    } = this.props;
-    return (
-      <a ref={el => {this.$el = el;}} {...others} className={'button' + (others.className ? ' ' + others.className : '')}>
-        {children}
-      </a>
-    );
-  }
-}
+export default Button
--- src/Picker/Picker.js
+++ src/Picker/Picker.js
@@ -37,7 +37,7 @@ const Picker = forwardRef(({
     } else if (list && list.length > 0) {
       initInstance();
     }
-  }, [show])
+  }, [show]) // eslint-disable-line
 
   function setDefault () {
     const defaultOpt = getDefaultOption();
