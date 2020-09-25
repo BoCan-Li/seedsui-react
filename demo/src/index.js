2976e9e1c7fd6d7c3a9c27f90a3a9fe7cd1c4394
wangmingzhu <wangmingzhu@waiqin365.com>
2020-09-25 17:44:03 +0800

    选地址支持异步省市区

--- demo/src/index.js
+++ demo/src/index.js
@@ -7,27 +7,144 @@ import {
 	Titlebar,
   Bridge,
   Container,
-  NumBox
+  InputDistrict
 } from '../../src';
 
 function Demo () {
-	const [value, setValue] = useState('');
-  function changeNum (e, val) {
-    setValue(val);
-  }
+// 获取街道
+function getStreet (districtId) {
+  return new Promise((resolve) => {
+    Bridge.showLoading();
+    setTimeout(() => {
+      Bridge.hideLoading();
+      resolve([
+        {
+          "parentid": districtId,
+          "name": "街道1",
+          "id": "1",
+        },
+        {
+          "parentid": districtId,
+          "name": "街道2",
+          "id": "2",
+        }
+      ])
+    }, 500);
+  })
+}
+
+const [value, setValue] = useState('');
+
+function onChange (e, value, selected) {
+  console.log(e.target)
+  console.log(value, selected)
+  setValue(value);
+}
+
+function getData () {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([{"name": "北京",
+      "id": "110000",
+      "children": [{
+        "name": "东城区",
+        "parentid": "110000",
+        "id": "110101",
+        "isLeaf": true
+      }, {
+        "name": "西城区",
+        "parentid": "110000",
+        "id": "110102",
+        "isLeaf": true
+      }, {
+        "name": "朝阳区",
+        "parentid": "110000",
+        "id": "110105",
+        "isLeaf": true
+      }, {
+        "name": "丰台区",
+        "parentid": "110000",
+        "id": "110106",
+        "isLeaf": true
+      }, {
+        "name": "石景山区",
+        "parentid": "110000",
+        "id": "110107",
+        "isLeaf": true
+      }, {
+        "name": "海淀区",
+        "parentid": "110000",
+        "id": "110108",
+        "isLeaf": true
+      }, {
+        "name": "门头沟区",
+        "parentid": "110000",
+        "id": "110109",
+        "isLeaf": true
+      }, {
+        "name": "房山区",
+        "parentid": "110000",
+        "id": "110111",
+        "isLeaf": true
+      }, {
+        "name": "通州区",
+        "parentid": "110000",
+        "id": "110112",
+        "isLeaf": true
+      }, {
+        "name": "顺义区",
+        "parentid": "110000",
+        "id": "110113",
+        "isLeaf": true
+      }, {
+        "name": "昌平区",
+        "parentid": "110000",
+        "id": "110114",
+        "isLeaf": true
+      }, {
+        "name": "大兴区",
+        "parentid": "110000",
+        "id": "110115",
+        "isLeaf": true
+      }, {
+        "name": "怀柔区",
+        "parentid": "110000",
+        "id": "110116",
+        "isLeaf": true
+      }, {
+        "name": "平谷区",
+        "parentid": "110000",
+        "id": "110117",
+        "isLeaf": true
+      }, {
+        "name": "密云区",
+        "parentid": "110000",
+        "id": "110118",
+        "isLeaf": true
+      }, {
+        "name": "延庆区",
+        "parentid": "110000",
+        "id": "110119",
+        "isLeaf": true
+      }]
+    }])
+    }, 2000);
+  })
+}
   return <Page>
 		<Header>
 			<Titlebar caption="SeedsUI"/>
 		</Header>
 		<Container>
-      <NumBox
-        className="lg"
-        digits={2}
-        min={0}
-        max={4}
-        required
+      <InputDistrict
         value={value}
-        onChange={changeNum}
+        onChange={onChange}
+        placeholder="请选择"
+        className="border-b"
+        pickerProps={{
+          getData: getData,
+          getStreet: getStreet
+        }}
       />
     </Container>
   </Page>
--- src/PickerDistrict/PickerDistrict.js
+++ src/PickerDistrict/PickerDistrict.js
@@ -1,18 +1,23 @@
-import React, { useContext, useState, useEffect, useRef, forwardRef } from 'react';
+import React, {useContext, useState, useEffect, useRef, forwardRef} from 'react';
 import {createPortal} from 'react-dom';
 import Context from '../Context/instance.js';
 import treeData from './instance.data.js';
-let streets = [] // 街道临时存储
+ 
+// 数据
+let currentData = null; // 省市区
+let streets = []; // 街道临时存储
 
 const PickerDistrict = forwardRef(({
     portal,
     data = treeData,
-    dataFormat = {
-      parentName: 'parentid',
-      idPropertyName: 'id',
-      namePropertyName: 'name',
-      childPropertyName: 'children'
-    },
+    dataFormat,
+    /*{
+      parentPropertyName: 'pId', // 将pId改成parentid
+      idPropertyName: 'id', // 将id改为id
+      namePropertyName: 'name', // 将name改为name
+      childPropertyName: 'child',  // 将child改为children
+    }*/
+    getData, // 异步获取数据
     split = '-',
 
     type = '', // province | city | district | street
@@ -32,15 +37,12 @@ const PickerDistrict = forwardRef(({
   const refElBody = useRef(null);
   // context
   const context = useContext(Context) || {};
-  let parentProperty = dataFormat.parentName || 'parentid';
-  let idProperty = dataFormat.idPropertyName || 'id';
-  let nameProperty = dataFormat.namePropertyName || 'name';
-  let childProperty = dataFormat.childPropertyName || 'children';
-  
+
   // 页签和列表
   let [tabs, setTabs] = useState([])
   let [tabIndex, setTabIndex] = useState(0)
   let [list, setList] = useState([])
+
   // 设置列表, 如果类型为省时, 则不显示直辖市, 类型为市时, 则不显示直辖市的区
   function isLeaf () { // 判断点击的是否是底层
     if (type === 'province') {
@@ -52,19 +54,21 @@ const PickerDistrict = forwardRef(({
     }
     return false;
   }
+
   // 获取父节点下所有的子节点
   function getParentChildren (parentid) {
     if (!parentid || parentid === '-1') {
-      return data;
+      return currentData;
     }
-    let parent = data.getDeepTreeNode(parentid, parentProperty, idProperty)
-    return parent[childProperty]
+    let parent = currentData.getDeepTreeNode(parentid)
+    return parent.children
   }
+
   // 根据key或者name获取tabs, property用于区分是key还是value比较
   let initTabs = [];
   function getTabs (values, property) {
     if (!values || !values.length) return null
-    if (property !== idProperty || property !== nameProperty) property = nameProperty
+    if (property !== 'id' || property !== 'name') property = 'name'
     initTabs = []
     var levels = 0 // 层级
     function getRow (parentid, list, value) {
@@ -72,49 +76,84 @@ const PickerDistrict = forwardRef(({
         // 模糊匹配
         if (item[property].indexOf(value) > -1 || value.indexOf(item[property]) > -1) {
           initTabs.push({
-            [parentProperty]: parentid,
+            parentid: parentid,
             ...item
           })
-          const children = item[childProperty];
+          const children = item.children;
           // 下一层级
           levels++
           if (values[levels] && children) {
-            getRow(item[idProperty], children, values[levels])
+            getRow(item.id, children, values[levels])
           }
         }
       }
     }
-    getRow('', data, values[0])
+    getRow('', currentData, values[0])
     // 如果省市区不对,则返回空
     return initTabs
   }
 
   // 数据初始化
   useEffect(() => {
-    console.log('地区初始化')
+    initCurrentData();
+  }, [data]); // eslint-disable-line
+  // 初始化数据
+  async function initCurrentData () {
+    // 初始化数据
+    currentData = null;
+    if (typeof getData === 'function') {
+      currentData = await getData();
+    } else if (Array.isArray(data)){
+      currentData = data;
+    }
+    if (currentData && currentData.length) {
+      if (dataFormat && (dataFormat.parentPropertyName || dataFormat.namePropertyName || dataFormat.idPropertyName || dataFormat.childPropertyName)) {
+        try {
+          let dataStr = JSON.stringify(currentData);
+          if (dataFormat.parentPropertyName) {
+            dataStr = dataStr.replace(new RegExp(dataFormat.parentPropertyName, 'igm'), 'parentid');
+          }
+          if (dataFormat.namePropertyName) {
+            dataStr = dataStr.replace(new RegExp(dataFormat.namePropertyName, 'igm'), 'name');
+          }
+          if (dataFormat.idPropertyName) {
+            dataStr = dataStr.replace(new RegExp(dataFormat.idPropertyName, 'igm'), 'id');
+          }
+          if (dataFormat.childPropertyName) {
+            dataStr = dataStr.replace(new RegExp(dataFormat.childPropertyName, 'igm'), 'parentid');
+          }
+          currentData = JSON.parse(dataStr);
+        } catch (error) {
+          currentData = null;
+        }
+      } else {
+        currentData = data;
+      }
+    }
+    
     // 初始化tabs
     if (selected && selected.length) {
-      initTabs = getTabs(selected.map((item) => {
+      initTabs = getTabs(selected.map((item) => { // eslint-disable-line
         return item.id
-      }), idProperty)
+      }), 'id')
     } else if (value) {
-      initTabs = getTabs(value.split(split || '-'), nameProperty)
+      initTabs = getTabs(value.split(split || '-'), 'name')
     }
     if (initTabs && initTabs.length) {
-      initTabs[initTabs.length - 1][nameProperty] = '请选择'
+      initTabs[initTabs.length - 1].name = '请选择'
     } else {
       initTabs[0] = {
-        [parentProperty]: '',
-        [idProperty]: '',
-        [nameProperty]: '请选择'
+        parentid: '',
+        id: '',
+        name: '请选择'
       }
     }
     // 初始化列表
-    let initList = getParentChildren(initTabs[initTabs.length - 1][parentProperty] || '')
+    let initList = getParentChildren(initTabs[initTabs.length - 1].parentid || '')
     setTabs(initTabs)
     setTabIndex(initTabs.length - 1)
     setList(initList)
-  }, [data]);
+  }
 
   // 如果列表发生变化, 则查找选中项
   useEffect(() => {
@@ -140,7 +179,7 @@ const PickerDistrict = forwardRef(({
       return
     }
     // 点击非街道
-    let children = getParentChildren(tab[parentProperty])
+    let children = getParentChildren(tab.parentid)
     if (children) {
       setTabIndex(index)
       setList(children)
@@ -155,7 +194,7 @@ const PickerDistrict = forwardRef(({
   function getOptionsStr (options) {
     let val = [];
     for (let option of options) {
-      val.push(option[nameProperty])
+      val.push(option.name)
     }
     return val.join(split)
   }
@@ -187,9 +226,9 @@ const PickerDistrict = forwardRef(({
     }
 
     // 没有children, 并且有街道请求, 则获取街道
-    if (!option[childProperty] || !option[childProperty].length) {
+    if (!option.children || !option.children.length) {
       if (!option.isStreet && getStreet) {
-        streets = await getStreet(option[idProperty])
+        streets = await getStreet(option.id)
         // 如果返回不是数组, 则认为返回错误
         if (streets instanceof Array === false) {
           return
@@ -207,11 +246,11 @@ const PickerDistrict = forwardRef(({
         })
         setList(streets)
         // 设置tabs
-        // spliceTabs[spliceTabs.length - 1][nameProperty] = option[nameProperty]
+        // spliceTabs[spliceTabs.length - 1].name = option.name
         spliceTabs.push({
-          [parentProperty]: option[idProperty],
-          [idProperty]: '',
-          [nameProperty]: '请选择'
+          parentid: option.id,
+          id: '',
+          name: '请选择'
         })
         setTabs(spliceTabs)
         setTabIndex(spliceTabs.length - 1)
@@ -225,16 +264,16 @@ const PickerDistrict = forwardRef(({
 
     // 补充请选择的空项
     spliceTabs.push({
-      [parentProperty]: '',
-      [idProperty]: '',
-      [nameProperty]: '请选择'
+      parentid: '',
+      id: '',
+      name: '请选择'
     });
 
     setTabs(spliceTabs)
     setTabIndex(tabLen)
     
     // 设置下级list
-    setList(option[childProperty])
+    setList(option.children)
   }
   return createPortal(
     <div
@@ -254,15 +293,15 @@ const PickerDistrict = forwardRef(({
         <div className="picker-district-tabbar">
           {tabs.map((tab, index) => {
             return <div onClick={() => onClickTab(tab, index)} className={`picker-district-tab${index === tabIndex ? ' active' : ''}`} key={index}>
-              {tab[nameProperty]}
+              {tab.name}
             </div>
           })}
         </div>
         <div className="picker-district-body" ref={refElBody}>
           {list && list.map((item, index) => {
-            return <div key={index} onClick={(e) => onClickOption(e, item)} className={`picker-district-option${tabs[tabIndex][idProperty] === item[idProperty] ? ' active' : ''}`}>
+            return <div key={index} onClick={(e) => onClickOption(e, item)} className={`picker-district-option${tabs[tabIndex].id === item.id ? ' active' : ''}`}>
               <div className="picker-district-option-icon"></div>
-              <div className="picker-district-option-caption">{item[nameProperty]}</div>
+              <div className="picker-district-option-caption">{item.name}</div>
             </div>
           })}
         </div>
