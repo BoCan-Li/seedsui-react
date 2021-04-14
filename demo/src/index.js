ca8eb7c974155b1218a9bce2f4213fb16a63a6a2
colaboy <49201650@qq.com>
2021-04-14 11:05:57 +0800

    修改首页

--- demo/src/index.js
+++ demo/src/index.js
@@ -13,22 +13,10 @@ import {
   MenuTiled
 } from '../../src'
 
-
-const menus = [
-  {id: '1', name: '测试数据1', parentid: '-1'},
-  {id: '2', name: '测试数据2', parentid: '-1'},
-  {id: 'a', name: '测试数据1-a', parentid: '1'},
-  {id: 'b', name: '测试数据1-b', parentid: '1'},
-  {id: 'I', name: '测试数据1-b-I', parentid: 'b'},
-  {id: 'II', name: '测试数据1-b-II', parentid: 'b'}
-];
-
 function Demo () {
-  const [selected, setSelected] = useState([{id: 'b', name: '测试数据1-b', parentid: '1'}])
   const [count, setCount] = useState(10)
 
   useEffect(() => {
-    console.log(new Date().diff(new Date('2023-04-10')))
   }, [])
 
   function handleCountDown () {
@@ -36,15 +24,9 @@ function Demo () {
     console.log(count)
   }
 
-  function onClickMenu (e, value, selected, data) {
-    console.log(e, value, selected, data);
-    setSelected(selected)
-  }
-
   return <Page>
 		<Header>
 			<Titlebar caption="标题"/>
-      <MenuTiled multiple list={menus} selected={selected} onChange={onClickMenu}/>
 		</Header>
 		<Container>
       {count}
